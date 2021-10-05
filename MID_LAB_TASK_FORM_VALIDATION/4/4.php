17775f523b6f33433e42c52e5cff37ac0b0ad759
Nafiz <Nafiz@DESKTOP-EO8S0A4>
2021-10-05 15:32:16 +0600

    4 done

--- MID_LAB_TASK_FORM_VALIDATION/4/4.php
+++ MID_LAB_TASK_FORM_VALIDATION/4/4.php
@@ -1,16 +1,10 @@
 <?php
 
 
-	if(isset($_REQUEST['g'])){
-		$g = $_REQUEST['g'];
-		if(empty($g))
-		{
-			echo 'Select someone \n';
-		}
-		else {
-			echo $g.' Selected \n';
-		}
+	if(isset($_POST['g'])){
+		$g = $_POST['g'];
 	}
+
 ?>
 
 
@@ -19,13 +13,13 @@
 	<title>Gender</title>
 </head>
 <body>
-	<form method="post" action="B.php">
+	<form method="post" action="4.php">
 		<b>Gender:<b> <br>
 			<input type="radio" name="g" value="male"> Male
 			<input type="radio" name="g" value="female"> Female
 			<input type="radio" name="g" value="other"> Other <br>
 		<input type="submit" name="submit" value="Submit">
-		<br><br><?php if(isset($_REQUEST['g'])){ echo "Gender : ".$g; }?>
+		<br><br><?php if(isset($_REQUEST['g'])){ echo "Gender : ".$g; } else {echo 'Select someone';}?>
 	</form>
 </body>
 </html>
