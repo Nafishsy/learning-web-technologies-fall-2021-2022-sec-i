29d6ca315d3156bef6c6d9b8b88a26d56f8b74a5
NAFIZ AHMED <19-41283-3@student.aiub.edu>
2021-11-24 20:56:49 +0600

    3

--- FINAL_LAB_PERFORMANCE_2/script.js
+++ FINAL_LAB_PERFORMANCE_2/script.js
@@ -27,3 +27,20 @@ function calculate() {
  var q = eval(p);
  document.getElementById("result").value = q;
 }
+
+function ajax(){
+	let name = document.getElementById('result').value;
+
+	let xhttp = new XMLHttpRequest();
+
+	xhttp.open('POST', 'calc.php', true);
+	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xhttp.send(result);
+	xhttp.onreadystatechange = function(){
+
+		if(this.readyState == 4 && this.status == 200){
+
+			document.getElementById('result').innerHTML = this.responseText;
+		}
+	}
+}
